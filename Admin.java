abe44cc6fe346cab7a084cd0a3e2a16fc04433a3
YapMY001 <1211110456@soffice.mmu.edu.my>
2024-06-13 01:59:19 +0800

    skeleton

---
+++ Admin.java
@@ -0,0 +1,16 @@
+public class Admin extends JFrame{
+    
+    void addCD() {
+        System.out.println("CD added");
+    }
+
+    void editCD() {
+        System.out.println("CD edited");
+    }
+
+    void deleteCD() {
+        System.out.println("CD deleted");
+    }
+
+    
+}
