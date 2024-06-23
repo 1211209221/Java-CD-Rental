287913e519ff8d64239a22d2bc8b6345ef140d08
tanys9572 <tanyansan123@gmail.com>
2024-06-23 16:13:52 +0800

    ok

--- AdminPanel.java
+++ AdminPanel.java
@@ -14,7 +14,30 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 import java.math.BigDecimal;
+
+class IconHeaderRenderer extends DefaultTableCellRenderer {
+    private ImageIcon icon;
+
+    public IconHeaderRenderer(ImageIcon icon) {
+        this.icon = icon;
+        setHorizontalAlignment(JLabel.CENTER);
+        setHorizontalTextPosition(JLabel.LEFT);
+        //Preserving original table style
+        setBackground(UIManager.getColor("TableHeader.background"));
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        setText((String) value);
+        setIcon(icon);
+        return this;
+    }
+} 
 
 public class AdminPanel extends JFrame{
     String username;
@@ -42,6 +65,7 @@ public class AdminPanel extends JFrame{
         };
         
         JPanel headerPanel = showHeader(menuFrame, "Admin Panel", username, backButtonAction);
+        headerPanel.setBackground(Color.LIGHT_GRAY);
         mainp.add(headerPanel, BorderLayout.NORTH);
 
         JPanel buttonPanel = createButtonPanel(menuFrame);
@@ -139,6 +163,10 @@ public class AdminPanel extends JFrame{
         allData = readCDData();
         String[][] dataArray = allData.toArray(new String[0][]);
 
+        int count = dataArray.length;
+        JLabel countresult = new JLabel(" Total CDs found: " + count);
+        searchButtonPanel.add(countresult, BorderLayout.SOUTH);
+
         // Format prices with RM currency
         for (String[] row : dataArray) {
             row[1] = "RM " + row[1];
@@ -147,11 +175,24 @@ public class AdminPanel extends JFrame{
         table = new JTable(dataArray, columnNames);
         table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         table.setDefaultEditor(Object.class, null);
-        table.getColumnModel().getColumn(0).setPreferredWidth(300);
-        table.getColumnModel().getColumn(1).setPreferredWidth(120);
-        table.getColumnModel().getColumn(2).setPreferredWidth(80);
-        table.getColumnModel().getColumn(3).setPreferredWidth(140);
-        table.getColumnModel().getColumn(4).setPreferredWidth(220);
+
+        // ImageIcon originalIcon = new ImageIcon("image/sort_icon.png");
+        // Image image = originalIcon.getImage().getScaledInstance(15, 15, Image.SCALE_SMOOTH);
+        // ImageIcon sortIcon = new ImageIcon(image);
+        // table.getTableHeader().setDefaultRenderer(new IconHeaderRenderer(sortIcon));
+
+        TableColumnModel columnModel = table.getColumnModel();
+        columnModel.getColumn(0).setPreferredWidth(300);
+        columnModel.getColumn(1).setPreferredWidth(120);
+        columnModel.getColumn(2).setPreferredWidth(80);
+        columnModel.getColumn(3).setPreferredWidth(140);
+        columnModel.getColumn(4).setPreferredWidth(220);
+
+        // // Enable sorting
+        // table.setModel(new DefaultTableModel(dataArray, columnNames));
+        // TableRowSorter<TableModel> sorter = new TableRowSorter<>(table.getModel());
+        // table.setRowSorter(sorter);
+        // reset();
 
         JScrollPane scrollPane = new JScrollPane(table);
         scrollPane.setBorder(BorderFactory.createCompoundBorder(
@@ -187,7 +228,6 @@ public class AdminPanel extends JFrame{
                         JComboBox<String> genreComboBox = new JComboBox<>(new String[]{"Comedy", "Crime", "Sci-Fi", "Drame", "Action", "Historical", "Music", "Classical", "Folk"}); 
                         genreComboBox.setSelectedItem(genre);
                         JTextField distributorField = new JTextField(distributor);
-
                        
                         infoPanel.add(new JLabel("CD Name: "));
                         infoPanel.add(cdNameField);
@@ -208,40 +248,40 @@ public class AdminPanel extends JFrame{
                         updateButton.addActionListener(new ActionListener() {
                             public void actionPerformed(ActionEvent e) {
                                 String newCdName = cdNameField.getText().trim();
-                                    String newPrice = priceField.getText().trim();
-                                    String newStock = stockField.getText().trim();
-                                    String newGenre = (String) genreComboBox.getSelectedItem();
-                                    String newDistributor = distributorField.getText().trim();
+                                String newPrice = priceField.getText().trim();
+                                String newStock = stockField.getText().trim();
+                                String newGenre = (String) genreComboBox.getSelectedItem();
+                                String newDistributor = distributorField.getText().trim();
 
-                                    if (newCdName.isEmpty() || newPrice.isEmpty() || newStock.isEmpty() || newDistributor.isEmpty()) {
-                                        JOptionPane.showMessageDialog(cdInfoDialog, "All fields are required.", "Error", JOptionPane.ERROR_MESSAGE);
-                                        return; // Stop further processing
-                                    }
+                                if (newCdName.isEmpty() || newPrice.isEmpty() || newStock.isEmpty() || newDistributor.isEmpty()) {
+                                    JOptionPane.showMessageDialog(cdInfoDialog, "All fields are required.", "Error", JOptionPane.ERROR_MESSAGE);
+                                    return; // Stop further processing
+                                }
 
-                                    //validate price and stock
-                                    double price;
-                                    try {
-                                        price = Double.parseDouble(newPrice);
-                                        if (price <= 0) {
-                                            JOptionPane.showMessageDialog(cdInfoDialog, "Price CANNOT be in zero/negative value.", "Error", JOptionPane.ERROR_MESSAGE);
-                                            return; 
-                                        }
-                                    } catch (NumberFormatException ex) {
-                                        JOptionPane.showMessageDialog(cdInfoDialog, "Invalid price format.", "Error", JOptionPane.ERROR_MESSAGE);
+                                //validate price and stock
+                                double price;
+                                try {
+                                    price = Double.parseDouble(newPrice);
+                                    if (price <= 0) {
+                                        JOptionPane.showMessageDialog(cdInfoDialog, "Price CANNOT be in zero/negative value.", "Error", JOptionPane.ERROR_MESSAGE);
                                         return; 
                                     }
+                                } catch (NumberFormatException ex) {
+                                    JOptionPane.showMessageDialog(cdInfoDialog, "Invalid price format.", "Error", JOptionPane.ERROR_MESSAGE);
+                                    return; 
+                                }
 
-                                    int stock;
-                                    try {
-                                        stock = Integer.parseInt(newStock);
-                                        if (stock < 0) {
-                                            JOptionPane.showMessageDialog(cdInfoDialog, "Stock must be a positive value.", "Error", JOptionPane.ERROR_MESSAGE);
-                                            return; 
-                                        }
-                                    } catch (NumberFormatException ex) {
-                                        JOptionPane.showMessageDialog(cdInfoDialog, "Invalid stock format.", "Error", JOptionPane.ERROR_MESSAGE);
+                                int stock;
+                                try {
+                                    stock = Integer.parseInt(newStock);
+                                    if (stock < 0) {
+                                        JOptionPane.showMessageDialog(cdInfoDialog, "Stock must be a positive value.", "Error", JOptionPane.ERROR_MESSAGE);
                                         return; 
                                     }
+                                } catch (NumberFormatException ex) {
+                                    JOptionPane.showMessageDialog(cdInfoDialog, "Invalid stock format.", "Error", JOptionPane.ERROR_MESSAGE);
+                                    return; 
+                                }
 
                                 int confirmUpdate = JOptionPane.showConfirmDialog(null, "Are you sure you want to update the changes?", "Confirm Updation", JOptionPane.YES_NO_OPTION);
                                 if (confirmUpdate == JOptionPane.YES_OPTION) {
@@ -589,7 +629,7 @@ public class AdminPanel extends JFrame{
             }
         } catch (IOException e) {
             e.printStackTrace();
-            JOptionPane.showMessageDialog(menuFrame, "Error reading passrecord file.", "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(menuFrame, "Error reading pastrecord file.", "Error", JOptionPane.ERROR_MESSAGE);
         }
 
 
@@ -701,37 +741,49 @@ public class AdminPanel extends JFrame{
 
     private void showFeeManagement(JFrame menuFrame) {
         //Create new Dialog
-        JDialog cdInfoDialog = new JDialog(menuFrame, "Fee Management", true);
-        cdInfoDialog.setSize(400, 300);
-        cdInfoDialog.setLocationRelativeTo(menuFrame);
+        JDialog feeDialog = new JDialog(menuFrame, "Fee Management", true);
+        feeDialog.setSize(400, 300);
+        feeDialog.setLocationRelativeTo(menuFrame);
+
+        //title panel 
+        JPanel titlePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        titlePanel.setBackground(Color.WHITE);
+        ImageIcon logoIcon = new ImageIcon("image/fee.png"); 
+        JLabel titleLabel = new JLabel("Fee Management", JLabel.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        titlePanel.add(new JLabel(logoIcon));
+        titlePanel.add(titleLabel);
+
         //Create new Panel
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
-        //Set button name
+        // Button panel for the buttons
+        JPanel buttonPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 0, 10, 0); // Add space between buttons
+        gbc.gridx = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        // Set button names
         JButton rentalFeeButton = new JButton("View Rental Fee");
         JButton penaltyFeeButton = new JButton("View Penalty Fee");
         JButton viewTotalRevenueButton = new JButton("View Total Revenue");
-        //Set button size
+
+        // Set button size
         Dimension buttonSize = new Dimension(200, 50);
         rentalFeeButton.setPreferredSize(buttonSize);
         penaltyFeeButton.setPreferredSize(buttonSize);
         viewTotalRevenueButton.setPreferredSize(buttonSize);
-        //Set button alignment
-        rentalFeeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        penaltyFeeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        viewTotalRevenueButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        // Add vertical glue before the buttons to center them on Y axis
-        buttonPanel.add(Box.createVerticalGlue());
-        //Add space between buttons
-        buttonPanel.add(rentalFeeButton);
-        buttonPanel.add(Box.createVerticalStrut(10));
-        buttonPanel.add(penaltyFeeButton);
-        buttonPanel.add(Box.createVerticalStrut(10));
-        buttonPanel.add(viewTotalRevenueButton);
-        // Add vertical glue after the buttons to center them on Y axis
-        buttonPanel.add(Box.createVerticalGlue());
+
+        // Add buttons to the button panel with constraints
+        gbc.gridy = 0;
+        buttonPanel.add(rentalFeeButton, gbc);
+        gbc.gridy = 1;
+        buttonPanel.add(penaltyFeeButton, gbc);
+        gbc.gridy = 2;
+        buttonPanel.add(viewTotalRevenueButton, gbc);
+
         // Add buttonPanel to the dialog
-        cdInfoDialog.add(buttonPanel, BorderLayout.CENTER);
+        feeDialog.add(buttonPanel, BorderLayout.CENTER);
+        feeDialog.add(titlePanel, BorderLayout.NORTH);
         //Event Listener for Fee Management
         rentalFeeButton.addActionListener(e -> {
             String currentFee = "";
@@ -746,10 +798,10 @@ public class AdminPanel extends JFrame{
                 String message = "The current rental fee is: RM" + formattedFee + ". Do you want to change it?";
                 Object[] options = {"Yes", "No"};
                 
-                int choice = JOptionPane.showOptionDialog(cdInfoDialog, message, "Rental Fee",
+                int choice = JOptionPane.showOptionDialog(feeDialog, message, "Rental Fee",
                         JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
                 if (choice == JOptionPane.YES_OPTION) {
-                    String newFee = JOptionPane.showInputDialog(cdInfoDialog, "Enter the new rental fee:");
+                    String newFee = JOptionPane.showInputDialog(feeDialog, "Enter the new rental fee:");
                     if (newFee != null)
                     {
                         // Validate the new fee
@@ -764,9 +816,9 @@ public class AdminPanel extends JFrame{
                             } catch (IOException writeFail) {
                                 writeFail.printStackTrace();
                             }
-                            JOptionPane.showMessageDialog(cdInfoDialog, "Rental fee successfully edited.", "Success", JOptionPane.INFORMATION_MESSAGE);
+                            JOptionPane.showMessageDialog(feeDialog, "Rental fee successfully edited.", "Success", JOptionPane.INFORMATION_MESSAGE);
                         } catch (NumberFormatException ex) {
-                            JOptionPane.showMessageDialog(cdInfoDialog, "Invalid fee format. Please enter a valid number.", "Error", JOptionPane.ERROR_MESSAGE);
+                            JOptionPane.showMessageDialog(feeDialog, "Invalid fee format. Please enter a valid number.", "Error", JOptionPane.ERROR_MESSAGE);
                         }
                     }
                 }
@@ -787,10 +839,10 @@ public class AdminPanel extends JFrame{
                 String message = "The current penalty fee is: RM" + formattedFee + ". Do you want to change it?";
                 Object[] options = {"Yes", "No"};
                 
-                int choice = JOptionPane.showOptionDialog(cdInfoDialog, message, "Penalty Fee",
+                int choice = JOptionPane.showOptionDialog(feeDialog, message, "Penalty Fee",
                         JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
                 if (choice == JOptionPane.YES_OPTION) {
-                    String newFee = JOptionPane.showInputDialog(cdInfoDialog, "Enter the new penalty fee:");
+                    String newFee = JOptionPane.showInputDialog(feeDialog, "Enter the new penalty fee:");
                     if (newFee != null)
                     {
                         try {
@@ -803,9 +855,9 @@ public class AdminPanel extends JFrame{
                             } catch (IOException writeFail) {
                                 writeFail.printStackTrace();
                             }
-                            JOptionPane.showMessageDialog(cdInfoDialog, "Penalty fee successfully edited.", "Success", JOptionPane.INFORMATION_MESSAGE);
+                            JOptionPane.showMessageDialog(feeDialog, "Penalty fee successfully edited.", "Success", JOptionPane.INFORMATION_MESSAGE);
                         } catch (NumberFormatException ex) {
-                            JOptionPane.showMessageDialog(cdInfoDialog, "Invalid fee format. Please enter a valid number.", "Error", JOptionPane.ERROR_MESSAGE);
+                            JOptionPane.showMessageDialog(feeDialog, "Invalid fee format. Please enter a valid number.", "Error", JOptionPane.ERROR_MESSAGE);
                         }
                     }
                 }
@@ -832,12 +884,12 @@ public class AdminPanel extends JFrame{
                 String message = "The Total Incurred Penalty Fees is RM " + oformattedFee + "\n" +
                                  "The Total Money Earned From Renting is RM " + formattedFee + "\n" +
                                  "Total Money Earned (including Penalty Fees) is RM " + totalformattedFee;
-                JOptionPane.showMessageDialog(cdInfoDialog, message, "Revenue Summary", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(feeDialog, message, "Revenue Summary", JOptionPane.INFORMATION_MESSAGE);
             } catch (IOException readfail) {
                 readfail.printStackTrace();
             }
         });
-        cdInfoDialog.setVisible(true);
+        feeDialog.setVisible(true);
     }
 
     //header
